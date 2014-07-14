fe53d0d28999007981cf1af2a335bd212aaf31df
Raman <raman@hike.in>
2014-07-14 13:19:07 +0530

    step 4

--- shortly/shortly.py
+++ shortly/shortly.py
@@ -42,8 +42,42 @@ class Shortly(object):
         t = self.jinja_env.get_template(template_name)
         return Response(t.render(context), mimetype='text/html')
 
+    def insert_url(self, url):
+        short_id = self.redis.get('reverse-url:' + url)
+        if short_id is not None:
+            return short_id
+        url_num = self.redis.incr('last-url-id')
+        short_id = base36_encode(url_num)
+        self.redis.set('url-target:' + short_id, url)
+        self.redis.set('reverse-url:' + url, short_id)
+        return short_id
+
+    def on_new_url(self, request):
+        error = None
+        url = ''
+        if request.method == 'POST':
+            url = request.form['url']
+            if not is_valid_url(url):
+                error = 'Please enter a valid URL'
+            else:
+                short_id = self.insert_url(url)
+                return redirect('/%s+' % short_id)
+        return self.render_template('new_url.html', error=error, url=url)
 
 
+def is_valid_url(url):
+    parts = urlparse.urlparse(url)
+    return parts.scheme in ('http', 'https')
+
+def base36_encode(number):
+    assert number >= 0, 'positive integer required'
+    if number == 0:
+        return '0'
+    base36 = []
+    while number != 0:
+        number, i = divmod(number, 36)
+        base36.append('0123456789abcdefghijklmnopqrstuvwxyz'[i])
+    return ''.join(reversed(base36))
 
 def create_app(redis_host='localhost', redis_port=6379, with_static=True):
     app = Shortly({
