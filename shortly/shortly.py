8958f4aa6902a95293d90862a139f660f39df10f
Raman <raman@hike.in>
2014-07-14 13:00:00 +0530

    step 3

--- shortly/shortly.py
+++ shortly/shortly.py
@@ -15,6 +15,11 @@ class Shortly(object):
         template_path = os.path.join(os.path.dirname(__file__), 'templates')
         self.jinja_env = Environment(loader=FileSystemLoader(template_path),
                                      autoescape=True)
+        self.url_map = Map([
+                            Rule('/', endpoint='new_url'),
+                            Rule('/<short_id>', endpoint='follow_short_link'),
+                            Rule('/<short_id>+', endpoint='short_link_details')
+                            ])
 
     def dispatch_request(self, request):
         return Response('Hello World!')
@@ -31,6 +36,14 @@ class Shortly(object):
     def render_template(self, template_name, **context):
         t = self.jinja_env.get_template(template_name)
         return Response(t.render(context), mimetype='text/html')
+
+    def dispatch_request(self, request):
+        adapter = self.url_map.bind_to_environ(request.environ)
+        try:
+            endpoint, values = adapter.match()
+            return getattr(self, 'on_' + endpoint)(request, **values)
+        except HTTPException, e:
+            return e
 
 
 def create_app(redis_host='localhost', redis_port=6379, with_static=True):
